c25233ecf56a52e64d0ec8d505a2477b06d9c8e8
AhmedAGadir <ahmed.gadir@gmail.com>
2021-04-08 11:10:42 +0100

    t2-3334  with angular template created

--- scripts/t2-import-plunker.sh
+++ scripts/t2-import-plunker.sh
@@ -9,4 +9,18 @@ T2_CREATE_ANGULAR="$T2_HOME/scripts/t2-create-angular.sh"
 
 source $T2_CREATE_ANGULAR TICKET="3334" TEMPLATE="angular"
 
+PROJECT_LOCATION="$T2_HOME/projects/t2-3334"
 
+cd $PROJECT_LOCATION
+
+# pwd
+
+mkdir import
+
+cp -r "/Users/ahmedgadir/Downloads/Project/" "$PROJECT_LOCATION/import"
+
+# sudo mount -o remount,rw "$PROJECT_DIR_PATH/src"
+
+chmod 777 *
+
+cp -rf "$PROJECT_LOCATION/import/" "$PROJECT_DIR_PATH/src"
