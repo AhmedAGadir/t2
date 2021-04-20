4c0bf4c4a0c9c744dd8a55fa0f4b77b8eea34cf6
AhmedAGadir <ahmed.gadir@gmail.com>
2021-04-20 14:08:24 +0100

    before cleanup

---
+++ scripts/t2-import-plunker-react.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# 1) create angular project
+# 2) create folder import
+# 3) fetch content of plunker and put in import folder
+# 4) override? copy from 
+
+T2_CREATE_REACT="$T2_HOME/scripts/t2-create-react.sh"
+
+read -p "Enter react ticket number:" TICKET_NUMBER
+
+source $T2_CREATE_REACT TICKET=$TICKET_NUMBER TEMPLATE="react"
+
+PROJECT_LOCATION="$T2_HOME/projects/t2-$TICKET_NUMBER"
+
+cd $PROJECT_LOCATION
+
+# pwd
+
+mkdir import
+
+cp -r "/Users/ahmedgadir/Downloads/project_react/" "$PROJECT_LOCATION/import"
+
+# sudo mount -o remount,rw "$PROJECT_DIR_PATH/src"
+# chmod - r 777 *
+# chmod -R 777 src
+
+ls -l 
+
+chmod -R 755 *
+
+# ls -l
+
+cd import
+
+# rm index.html systemjs-angular-loader.js systemjs.config.js main.ts
+
+cd ..
+
+rm import
+
+#  didnt work
+# cp -rf "$PROJECT_LOCATION/import/" "$PROJECT_DIR_PATH/src"
+# mv -v $PROJECT_LOCATION/import/* $PROJECT_LOCATION/src
+# mv -f $PROJECT_LOCATION/import/* $PROJECT_LOCATION/src
+
+# works
+rsync -a $PROJECT_LOCATION/import/ $PROJECT_LOCATION/src
+rm -rf $PROJECT_LOCATION/import/*
+
+git add . 
+git commit -m 'update'
+git push
