db2ce11ebaab39f8a6f7e33f69ef709d8f4cdeb0
AhmedAGadir <ahmed.gadir@gmail.com>
2021-05-24 13:43:43 +0100

    update

--- projects/t2-test-react/src/index.js
+++ projects/t2-test-react/src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+'use strict';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const GridExample = () => {
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+
+    const updateData = (data) => {
+      var fakeServer = createFakeServer(data);
+      var datasource = createServerSideDatasource(fakeServer);
+      params.api.setServerSideDatasource(datasource);
+    };
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <div
+        id="myGrid"
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+        className="ag-theme-alpine-dark"
+      >
+        <AgGridReact
+          defaultColDef={{
+            flex: 1,
+            minWidth: 100,
+          }}
+          rowModelType={'serverSide'}
+          onGridReady={onGridReady}
+        >
+          <AgGridColumn field="athlete" minWidth={220} />
+          <AgGridColumn field="country" minWidth={200} />
+          <AgGridColumn field="year" />
+          <AgGridColumn field="sport" minWidth={200} />
+          <AgGridColumn field="gold" />
+          <AgGridColumn field="silver" />
+          <AgGridColumn field="bronze" />
+        </AgGridReact>
+      </div>
+    </div>
+  );
+};
+
+function createServerSideDatasource(server) {
+  return {
+    getRows: function (params) {
+      console.log('[Datasource] - rows requested by grid: ', params.request);
+      var response = server.getData(params.request);
+      setTimeout(function () {
+        if (response.success) {
+          params.success({ rowData: response.rows });
+        } else {
+          params.fail();
+        }
+      }, 500);
+    },
+  };
+}
+function createFakeServer(allData) {
+  return {
+    getData: function (request) {
+      var requestedRows = allData.slice();
+      return {
+        success: true,
+        rows: requestedRows,
+      };
+    },
+  };
+}
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
