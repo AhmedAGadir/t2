3cfcbf5c87a7dd59886daec05c41dff9416ca451
AhmedAGadir <ahmed.gadir@gmail.com>
2021-05-25 09:50:29 +0100

    update

--- projects/t2-test-vanilla/src/index.js
+++ projects/t2-test-vanilla/src/index.js
@@ -1,27 +1,38 @@
-import "./styles.css";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import "ag-grid-enterprise";
 import * as agGrid from "ag-grid-community";
 
-// specify the columns
-const columnDefs = [
-  { field: "make" },
-  { field: "model" },
-  { field: "price" }
-];
-
-// let the grid know which columns to use
-const gridOptions = {
-  columnDefs: columnDefs
+var gridOptions = {
+  columnDefs: [
+    { field: 'athlete', minWidth: 150 },
+    { field: 'age', maxWidth: 90 },
+    { field: 'country', minWidth: 150 },
+    { field: 'year', maxWidth: 90 },
+    { field: 'date', minWidth: 150 },
+    { field: 'sport', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 100,
+  },
+  enableRangeSelection: true,
 };
 
-// lookup the container we want the Grid to use
-const eGridDiv = document.querySelector('#myGrid');
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(gridDiv, gridOptions);
 
-// create the grid passing in the div to use together with the columns &amp; data we want to use
-new agGrid.Grid(eGridDiv, gridOptions);
-
-agGrid.simpleHttpRequest({ url: 'https://www.ag-grid.com/example-assets/row-data.json' }).then(data => {
-  gridOptions.api.setRowData(data);
+  agGrid
+    .simpleHttpRequest({
+      url: 'https://www.ag-grid.com/example-assets/olympic-winners.json',
+    })
+    .then(function (data) {
+      gridOptions.api.setRowData(data);
+    });
 });
