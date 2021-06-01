c3deae3bea788486f43eff4e24d1badb23737f25
AhmedAGadir <ahmed.gadir@gmail.com>
2021-06-01 11:56:38 +0100

    update

--- projects/t2-test-vanilla-ahmed/src/index.js
+++ projects/t2-test-vanilla-ahmed/src/index.js
@@ -5,22 +5,24 @@ import * as agGrid from "ag-grid-community";
 
 var gridOptions = {
   columnDefs: [
-    { field: 'athlete', minWidth: 220 },
-    { field: 'country', minWidth: 200 },
-    { field: 'year' },
-    { field: 'sport', minWidth: 200 },
-    { field: 'gold' },
-    { field: 'silver' },
-    { field: 'bronze' },
+    { field: 'country', rowGroup: true },
+    { field: 'year', rowGroup: true },
+    { field: 'sport' },
+    { field: 'athlete' },
+    { field: 'total' },
   ],
-
   defaultColDef: {
     flex: 1,
     minWidth: 100,
+    filter: true,
+    sortable: true,
+    resizable: true,
   },
-
-  // use the server-side row model instead of the default 'client-side'
-  rowModelType: 'serverSide',
+  autoGroupColumnDef: {
+    minWidth: 200,
+  },
+  enableRangeSelection: true,
+  animateRows: true,
 };
 
 // setup the grid after the page has finished loading
@@ -33,48 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
       url: 'https://www.ag-grid.com/example-assets/olympic-winners.json',
     })
     .then(function (data) {
-      // setup the fake server with entire dataset
-      var fakeServer = createFakeServer(data);
-
-      // create datasource with a reference to the fake server
-      var datasource = createServerSideDatasource(fakeServer);
-
-      // register the datasource with the grid
-      gridOptions.api.setServerSideDatasource(datasource);
+      gridOptions.api.setRowData(data);
     });
 });
-
-function createServerSideDatasource(server) {
-  return {
-    getRows: function (params) {
-      console.log('[Datasource] - rows requested by grid: ', params.request);
-
-      // get data for request from our fake server
-      var response = server.getData(params.request);
-
-      // simulating real server call with a 500ms delay
-      setTimeout(function () {
-        if (response.success) {
-          // supply rows for requested block to grid
-          params.success({ rowData: response.rows });
-        } else {
-          params.fail();
-        }
-      }, 500);
-    },
-  };
-}
-
-function createFakeServer(allData) {
-  return {
-    getData: function (request) {
-      // take a copy of the data to return to the client
-      var requestedRows = allData.slice();
-
-      return {
-        success: true,
-        rows: requestedRows,
-      };
-    },
-  };
-}
